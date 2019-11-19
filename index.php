6a0188b6c84aa417602d3fbf431dd69bc86795d7
srlechuga18 <aledeleon222@hotmail.com>
2019-11-19 09:27:32 -0600

    api

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php 
-phpinfo();
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    http_response_code(200);
+    echo 'GET';
+}elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    http_response_code(200);
+    echo 'POST';
+}else {
+    http_response_code(405);
+}
+
 ?>
